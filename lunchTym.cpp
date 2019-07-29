d3f266e1d16967a65c965e291a866d964c885c35
Kavita Jatwar <kavitajatwar970@gmail.com>
2019-07-29 19:23:42 +0530

    code

---
+++ lunchTym.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+
+ll sum(ll a){
+    ll total=0;
+    while(a>0){
+    total+=a%10;
+    a=a/10;}return total;
+}
+int main()
+{
+    ll T;
+    cin>>T;
+    while(T--)
+    {
+        ll n,i,j,prod,m=0;
+        cin>>n;
+        ll arr[n];
+        for(i=0;i<n;i++)
+            cin>>arr[i];
+
+        for(i=0;i<n-1;i++){
+            for(j=i+1;j<n;j++){
+                prod= sum(arr[i]*arr[j]);
+                if(prod>m){
+                    m=prod;
+                }
+            }
+        }cout<<m<<endl;
+    }
+    return 0;
+}
+
